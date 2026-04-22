48ba40a94037acf7cea57d05d383bea76a3f5d5a
Nathan Contreras <ncontreras@ucsb.edu>
2026-04-22 02:07:11 -0700

    Created and filled entity, repository, and db files for UCSBDiningCommonsMenuItem table

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemRepository.java
@@ -0,0 +1,8 @@
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RepositoryRestResource(exported = false)
+public interface UCSBDiningCommonsMenuItemRepository
+    extends CrudRepository<UCSBDiningCommonsMenuItem, Long> {}
