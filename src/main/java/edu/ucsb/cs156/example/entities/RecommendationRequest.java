2fc3e25f163579bcd34e6368b55f8a08ee58a27b
Abhijeet Dutta <abhijeet@ucsb.edu>
2026-04-28 23:02:48 -0700

    create database table for RecommendationRequest pt2

--- src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "recommendationrequests")
+@Entity(name = "RecommendationRequest")
 public class RecommendationRequest {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
